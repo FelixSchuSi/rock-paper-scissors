9ee3698a4936305215e2c70f5ca174f07f8e8aec
Felix Schulze Sindern <felixs@fh-muenster.de>
2023-12-07 23:05:24 +0100

    Refactor

--- frontend/src/player-joined-room-callback.ts
+++ frontend/src/player-joined-room-callback.ts
@@ -1,12 +1,11 @@
 import { STATE } from ".";
 import { Player } from "../../shared/src/types/player";
 import { Room } from "../../shared/src/types/room";
-import {
-  WebSocketMessage,
-  WebSocketMessageType,
-} from "../../shared/src/web-socket-message";
+import { WebSocketMessage } from "../../shared/src/web-socket-message";
+import { rematch } from "./rematch";
 import { webSocketMessageHandler } from "./web-socket-message-handler";
 
+let socket: WebSocket;
 export function playerJoinedRoomCallback(apiResponse: {
   room: Room;
   player: Player;
@@ -19,12 +18,13 @@ export function playerJoinedRoomCallback(apiResponse: {
   document.querySelector(".invite-screen")!.classList.remove("hidden");
 
   document.querySelector(".start-screen")!.classList.add("hidden");
-  const socket = new WebSocket(
+  socket = new WebSocket(
     `${location.protocol === "https:" ? "wss://" : "ws://"}${
       location.hostname
-    }${location.port !== "" ? `:${location.port}` : ""}/join-room`
+    }${location.port !== "" ? `:${location.port}` : ""}/upgrade`
   );
   socket.addEventListener("message", webSocketMessageHandler);
+
   window.addEventListener("item-placed", (e) => {
     const message: WebSocketMessage = {
       type: "PLAYER_PLACED_ITEM",
@@ -35,21 +35,16 @@ export function playerJoinedRoomCallback(apiResponse: {
     console.log("clientside sending message", message);
     socket.send(JSON.stringify(message));
   });
-  document.querySelectorAll(".restart-btn")!.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const message: WebSocketMessage = {
-        type: WebSocketMessageType.REMATCH_REQUEST,
-        data: {},
-        fromPlayerId: STATE.playerId,
-        room: STATE.room!,
-      };
-      socket.send(JSON.stringify(message));
-
-      document.querySelector(".invite-screen")!.classList.remove("hidden");
-      document
-        .querySelectorAll(".final-screen")
-        .forEach((e) => e.classList.add("hidden"));
-      document.querySelector("canvas")!.classList.remove("blurry");
-    });
-  });
 }
+
+document.querySelectorAll(".restart-btn")!.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    socket.close();
+    rematch();
+    document.querySelector(".invite-screen")!.classList.remove("hidden");
+    document
+      .querySelectorAll(".final-screen")
+      .forEach((e) => e.classList.add("hidden"));
+    document.querySelector("canvas")!.classList.remove("blurry");
+  });
+});
